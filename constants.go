84b603fd1ec6d341e9dee9c8c67db4c5d05822ad
Prithvi Ramesh <prithvi@platform9.com>
2017-10-09 11:58:44 -0700

    constant example

--- constants.go
+++ constants.go
@@ -4,3 +4,15 @@ import "fmt"
 import "math"
 
 const s string = "constant"
+
+func main(){
+
+	fmt.Println(s);
+	const n = 5000000;
+	const d = 3e20 / n;
+	fmt.Println(d);
+
+	fmt.Println(int64(d));
+	fmt.Println(math.Sin(n));
+
+}
